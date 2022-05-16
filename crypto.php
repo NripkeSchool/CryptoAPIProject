6005f4812d6ba7ad96fa1e9669a1e59076aa78d2
Ripke <nripke@friendsbalt.org>
2022-05-16 12:59:34 -0400

    Did more work on implementing api

--- crypto.php
+++ crypto.php
@@ -5,7 +5,8 @@
         echo("Didn't set");
     }
 
-    $ch = curl_init("https://api.polygon.io/v2/aggs/ticker/X:BTCUSD/range/12/second/2021-07-22/2021-07-22?apiKey=NxKBLUenlfb_6QT0v_8VpBG4OrS6fANx");
+    $apiKey = "NxKBLUenlfb_6QT0v_8VpBG4OrS6fANx";
+    $ch = curl_init("https://api.polygon.io/v2/aggs/ticker/X:BTCUSD/range/12/second/2021-07-22/2021-07-22?apiKey=".$apiKey);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     $data = curl_exec($ch);
--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function postRequest(request, loadcb) {
     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhttp.send(request);
 }
+
+function testing()
+{
+    const d = new Date();
+    let month=d.getMonth();
+    let day=d.getDate();
+    if (month < 10)
+    {
+        month = "0"+month;
+    }
+    if (day < 10)
+    {
+        day = "0"+day;
+    }
+    let request = 'getData='+d.getFullYear()+"-"+month+"-"+day;
+    console.log(request);
+    function callback() {document.getElementById("info").innerHTML = this.responseText;}
+
+    postRequest(request, callback);
+}
